91382fa0f2e4118e50b8827debf97636ab150737
Jetug <ksergeev01@gmail.com>
2023-09-01 00:17:31 +0300

    hud test

--- src/main/java/com/jetug/chassis_addon/client/hud/HeatRenderer.java
+++ src/main/java/com/jetug/chassis_addon/client/hud/HeatRenderer.java
@@ -1,5 +1,6 @@
 package com.jetug.chassis_addon.client.hud;
 
+import com.jetug.chassis_core.common.util.helpers.MathHelper;
 import com.jetug.generated.resources.Textures;
 import com.mojang.blaze3d.systems.RenderSystem;
 import com.mojang.blaze3d.vertex.PoseStack;
@@ -9,35 +10,37 @@ import net.minecraftforge.client.gui.IIngameOverlay;
 
 import static com.jetug.chassis_addon.common.utils.ChassisUtils.getSteamChassis;
 import static com.jetug.chassis_addon.common.utils.ChassisUtils.isWearingSteamChassis;
+import static com.jetug.chassis_core.common.util.helpers.MathHelper.*;
 
 public class HeatRenderer implements IIngameOverlay
 {
-    public static final int BAR_HEIGHT = 52;
-    public static final int BAR_WIDTH = 95;
+    public static final int BAR_OFFSET_X = 95;
+    public static final int BG_WIDTH = 44;
+    public static final int BG_HEIGHT = 50;
 
     @Override
     public void render(ForgeIngameGui gui, PoseStack poseStack, float partialTick, int width, int height) {
         if (!isWearingSteamChassis()) return;
 
-        RenderSystem.setShaderTexture(0, Textures.GUI_HUD);
-        RenderSystem.enableBlend();
+        //RenderSystem.setShaderTexture(0, Textures.GUI_HUD);
+        //RenderSystem.enableBlend();
         var minecraft = Minecraft.getInstance();
         var screenWidth  = minecraft.getWindow().getGuiScaledWidth();
         var screenHeight = minecraft.getWindow().getGuiScaledHeight();
 
-        var x = screenWidth / 2 + BAR_WIDTH;
-        var y = height - BAR_HEIGHT;
+        var x = screenWidth / 2 + BAR_OFFSET_X;
+        var y = height - BG_HEIGHT;
 
-        gui.blit(poseStack, x, y, 0, 0, 9, BAR_HEIGHT);
+        //gui.blit(poseStack, x, y, 0, 0, BG_WIDTH, BG_HEIGHT);
 
-        //RenderSystem.setShaderTexture(0, ICONS_LOCATION);
         var armor = getSteamChassis(minecraft.player);
-        int heat = armor.heatController.getHeatInPercent() / 2;
+        int heat = armor.heatController.getHeatInPercent();
+        int arrowNumber = (int)getPercentOf(17, heat);
 
-        var offset = 50 - heat + 1;
+        var offset = 26 * arrowNumber;
 
-        gui.blit(poseStack, x + 1, y + offset, 10,  offset, 7, heat);
-
+        RenderSystem.setShaderTexture(0, Textures.GUI_ARROW);
+        gui.blit(poseStack, x + 10, y + 8, 0,  0, 25, 17);
     }
 
 }
